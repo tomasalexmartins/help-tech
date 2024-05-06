559ac19be168dad6e8574e4cf7978a6a406d9610
Tomás Martins <tomartins@deloitte.pt>
2024-05-06 16:36:33 +0100

    RecoverPasswordPage

---
+++ HelpTech/HelpTech/Presentation/RecoverPasswordPage/RecoverPasswordPageView.swift
@@ -0,0 +1,59 @@
+//
+//  RecoverPasswordPageView.swift
+//  HelpTech
+//
+//  Created by Martins, Tomas Alexandre on 06/05/2024.
+//
+
+import SwiftUI
+import UI
+
+protocol RecoverPasswordPageViewProtocol: View { }
+
+struct RecoverPasswordPageView<ViewModel:RecoverPasswordPageViewModelProtocol>: RecoverPasswordPageViewProtocol {
+    
+    @StateObject
+    var viewModel: ViewModel
+    
+    var backButton: some View {
+        VStack(alignment: .leading) {
+            BackButtonView(action: .constant({}))
+        }
+        .frame(height: 65)
+        .padding(.trailing, 330)
+        .padding(.top, 15)
+    }
+    
+    var emailView: some View {
+        CustomTextField(viewModel: viewModel.emailTextField)
+            .padding(.horizontal, 40)
+    }
+    
+    var emailButton: some View {
+        VStack {
+            CustomButtonView(text: .constant("Recuperar"), isEnabled:
+                                $viewModel.isEnabled, action: .constant({}))
+        }
+        .padding(.horizontal, 40)
+        .padding(.top, 15)
+        .frame(height: 65)
+    }
+    
+    var body: some View {
+        VStack() {
+            backButton
+            VStack() {
+                Spacer().frame(height: 200)
+                emailView
+                emailButton
+                Spacer()
+            }
+            .navigationBarBackButtonHidden(true)
+        }
+        .background(.gray.opacity(0.3))
+    }
+}
+
+#Preview {
+    RecoverPasswordPageView<RecoverPasswordPageViewModel>(viewModel: .init())
+}
---
+++ HelpTech/HelpTech/Presentation/RecoverPasswordPage/RecoverPasswordPageViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  RecoverPasswordPageViewModel.swift
+//  HelpTech
+//
+//  Created by Martins, Tomas Alexandre on 06/05/2024.
+//
+
+import Foundation
+import SwiftUI
+import UI
+
+protocol RecoverPasswordPageViewModelProtocol: ViewModelProtocol {
+    var emailTextField: CustomTextFieldViewModel { get set }
+    var isEnabled: Bool { get set }
+    
+}
+
+class RecoverPasswordPageViewModel: RecoverPasswordPageViewModelProtocol {
+    
+    
+    @Published
+    var emailTextField: CustomTextFieldViewModel = .init(title: "E-mail",
+                                                         placeholder: "e-mail",
+                                                         text: "")
+    
+    @Published
+    var isEnabled = true
+    
+    init() {
+    }
+    
+}
