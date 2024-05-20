32a73d2005fef9aeadbb1f8793b5d2a5a4d5c766
Tomás Martins <tomartins@deloitte.pt>
2024-05-20 17:29:02 +0100

    Missed Model Class

--- HelpTech/HelpTech/Presentation/PartsPage/Part.swift
+++ HelpTech/HelpTech/Presentation/PartsPage/Part.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import Common
+
+public struct Part: Hashable, Decodable {
+
+    public let id: String?
+    public var name: String?
+    public var supplier: Supplier?
+    public var quantity: Int?
+    public var price: Double?
+    public var description: String?
+    public var stock: Bool?
+
+    public init(id: String? = nil,
+                name: String? = nil,
+                supplier: Supplier? = nil,
+                quantity: Int? = nil,
+                price: Double? = nil,
+                description: String? = nil,
+                stock: Bool? = nil) {
+
+        self.id = id
+        self.name = name
+        self.supplier = supplier
+        self.quantity = quantity
+        self.price = price
+        self.description = description
+        self.stock = stock
+    }
+
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+}
