df9ff14f8c14bb8337247f3ab2bf7046bd86b42c
Tomás Martins <tomartins@deloitte.pt>
2024-05-20 17:28:42 +0100

    NewEditClientPage

--- HelpTech/HelpTech/Presentation/NewEditClientPage/NewEditClientPageView.swift
+++ HelpTech/HelpTech/Presentation/NewEditClientPage/NewEditClientPageView.swift
@@ -5,4 +5,94 @@
 //  Created by Martins, Tomas Alexandre on 19/05/2024.
 //
 
-import Foundation
+import SwiftUI
+import UI
+
+protocol NewEditClientPageViewProtocol: View { }
+
+struct NewEditClientPageView<ViewModel:NewEditClientPageViewModelProtocol>: NewEditClientPageViewProtocol {
+    
+    @StateObject
+    var viewModel: ViewModel
+    
+    var backButton: some View {
+        VStack(alignment: .leading) {
+            BackButtonView(action: .constant({viewModel.back()}))
+        }
+        .frame(height: 65)
+        .padding(.trailing, 330)
+        .padding(.top, 15)
+    }
+    
+    var titleView: some View {
+        VStack {
+            Text(viewModel.title)
+                .font(.title3)
+                .bold()
+        }
+        .padding(.bottom, 20)
+    }
+    
+    var nameView: some View {
+        CustomTextField(viewModel: viewModel.nameTextField)
+            .padding(.horizontal, 40)
+            .padding(.top, 15)
+    }
+    
+    var emailView: some View {
+        CustomTextField(viewModel: viewModel.emailTextField)
+            .padding(.horizontal, 40)
+            .padding(.top, 15)
+    }
+    
+    var cellphoneView: some View {
+        CustomTextField(viewModel: viewModel.cellphoneTextField)
+            .padding(.horizontal, 40)
+            .padding(.top, 15)
+    }
+    
+    var addressView: some View {
+        CustomTextField(viewModel: viewModel.addressTextField)
+            .padding(.horizontal, 40)
+            .padding(.top, 15)
+    }
+    
+    var nifView: some View {
+        CustomTextField(viewModel: viewModel.nifTextField)
+            .padding(.horizontal, 40)
+            .padding(.top, 15)
+    }
+    
+    
+    var newEditClientButton: some View {
+        VStack {
+            CustomButtonView(text: .constant(viewModel.btnText), isEnabled:
+                    .constant(true), action: .constant({viewModel.newEditClient()}))
+        }
+        .padding(.horizontal, 40)
+        .padding(.top, 15)
+        .frame(height: 65)
+    }
+    
+    var body: some View {
+        VStack() {
+            backButton
+            titleView
+            nameView
+            emailView
+            cellphoneView
+            addressView
+            nifView
+            VStack() {
+                newEditClientButton
+                Spacer()
+            }
+            .navigationBarBackButtonHidden(true)
+        }
+        .background(.gray.opacity(0.3))
+    }
+}
+
+#Preview {
+    NewEditClientPageView<NewEditClientPageViewModel>(viewModel: .init())
+}
--- HelpTech/HelpTech/Presentation/NewEditClientPage/NewEditClientPageViewModel.swift
+++ HelpTech/HelpTech/Presentation/NewEditClientPage/NewEditClientPageViewModel.swift
@@ -6,3 +6,72 @@
 //
 
 import Foundation
+import SwiftUI
+import UI
+import Domain
+
+protocol NewEditClientPageViewModelProtocol: ViewModelProtocol {
+    var title: String { get set }
+    var btnText: String { get set }
+    var client: Client { get set }
+    var nameTextField: CustomTextFieldViewModel { get set }
+    var emailTextField: CustomTextFieldViewModel { get set }
+    var cellphoneTextField: CustomTextFieldViewModel { get set }
+    var addressTextField: CustomTextFieldViewModel { get set }
+    var nifTextField: CustomTextFieldViewModel { get set }
+    
+    func newEditClient()
+    func back()
+}
+
+class NewEditClientPageViewModel: NewEditClientPageViewModelProtocol {
+    
+    private var flagNewEdit: String = ""
+    
+    @Published
+    public var title: String = "Teste Texto"
+    
+    @Published
+    public var btnText: String = "Teste btn"
+    
+    @Published
+    public var client: Client = .init(id: nil,
+                                      firstname: nil,
+                                      lastname: nil,
+                                      email: nil,
+                                      cellphone: nil,
+                                      address: nil,
+                                      nif: nil)
+    
+    @Published
+    var nameTextField: CustomTextFieldViewModel = .init(title: "Nome",
+                                                        placeholder: "nome...",
+                                                        text: "")
+    @Published
+    var emailTextField: CustomTextFieldViewModel = .init(title: "E-mail",
+                                                         placeholder: "e-mail...",
+                                                         text: "")
+    @Published
+    var cellphoneTextField: CustomTextFieldViewModel = .init(title: "Telefone",
+                                                             placeholder: "telefone...",
+                                                             text: "")
+    @Published
+    var addressTextField: CustomTextFieldViewModel = .init(title: "Morada",
+                                                           placeholder: "morada...",
+                                                           text: "")
+    @Published
+    var nifTextField: CustomTextFieldViewModel = .init(title: "NIF",
+                                                       placeholder: "nif...",
+                                                       text: "")
+    
+    init() {
+    }
+    
+    func newEditClient() {
+        
+    }
+    
+    func back() {
+        
+    }
+}
